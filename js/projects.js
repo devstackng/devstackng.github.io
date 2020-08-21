ee0bef364e28598474fb1975afce015cbb029e1c
Prince Abalogu <chineduabalogu@yahoo.com>
2020-08-21 18:54:49 +0100

    bump copyright year

--- js/projects.js
+++ js/projects.js
@@ -3,58 +3,58 @@ const vueApp = new Vue({
   data: { 
     projects: [
       {
-        image: "/img/projects/hiis-claims.jpg",
+        image: "img/projects/hiis-claims.jpg",
         name: "Health Insurance Innovative Systems",
         categories_id: [1, 2],
-        url: ""
+        url: "https://hiisnigeria.github.io/"
       },
       {
         image: "img/projects/hiis-mobile.jpg",
         name: "HIIS Mobile Application",
         categories_id: [2],
-        url: ""
+        url: "https://hiisnigeria.github.io/"
       },
       {
         image: "img/projects/feedintel.jpg",
         name: "Feedintel",
         categories_id: [1, 2],
-        url: ""
+        url: "http://feedintel.live/"
       },
       {
         image: "img/projects/hsdf.jpg",
         name: "EFSS - Electronic File Sharing System",
         categories_id: [1, 4],
-        url: ""
+        url: "http://hsdf-stage.herokuapp.com/"
       },
       {
         image: "img/projects/healthjobber.jpg",
         name: "HealthJobber",
         categories_id: [1],
-        url: ""
+        url: "http://hjstage.herokuapp.com/"
       },
       {
         image: "img/projects/hj-mobile.jpg",
         name: "HJ Mobile",
         categories_id: [2],
-        url: ""
+        url: "http://hjstage.herokuapp.com/"
       },
       {
         image: "img/projects/gaps-flyer.png",
         name: "GAPS",
         categories_id: [1, 2],
-        url: ""
+        url: "https://www.gaps.com.ng/"
       },
       {
-        image: "img/projects/filterland-crm.png",
+        image: "img/projects/filterland-flyer.png",
         name: "Filterland CRM",
         categories_id: [1, 3],
-        url: ""
+        url: "https://demo.filterlandindustries.com/"
       },
       {
         image: "img/projects/amina-flyer.png",
         name: "Amina.ai",
         categories_id: [1, 2],
-        url: ""
+        url: "https://amina.ai/"
       }
    ],
    categories: [
